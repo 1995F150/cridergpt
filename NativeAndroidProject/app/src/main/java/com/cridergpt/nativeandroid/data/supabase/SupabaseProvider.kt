9e082b8e6ae24bc818236289063e1a381a6e8e4c
Jessie Crider (CriderGPT) <jessiecrider3@gmail.com>
2026-03-04 16:36:53 -0500

    Fix Android build by removing postgrest extension dependency usage

--- NativeAndroidProject/app/src/main/java/com/cridergpt/nativeandroid/data/supabase/SupabaseProvider.kt
+++ NativeAndroidProject/app/src/main/java/com/cridergpt/nativeandroid/data/supabase/SupabaseProvider.kt
@@ -1,7 +1,6 @@
 package com.cridergpt.nativeandroid.data.supabase
 
 import io.github.jan.supabase.createSupabaseClient
-import io.github.jan.supabase.postgrest.Postgrest
 
 /**
  * Native Supabase client wiring requested by user.
@@ -10,7 +9,5 @@ object SupabaseProvider {
     val client = createSupabaseClient(
         supabaseUrl = SupabaseConfig.URL,
         supabaseKey = SupabaseConfig.PUBLISHABLE_KEY
-    ) {
-        install(Postgrest)
-    }
+    )
 }
--- NativeAndroidProject/app/src/main/java/com/cridergpt/nativeandroid/data/todo/TodoItem.kt
+++ NativeAndroidProject/app/src/main/java/com/cridergpt/nativeandroid/data/todo/TodoItem.kt
@@ -4,6 +4,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class TodoItem(
-    val id: Int,
+    val id: String,
     val name: String
 )
